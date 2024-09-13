260f19d37d189e0f2c2717363881c6e8b6726299
kimcarrington <165577489+kimcarrington@users.noreply.github.com>
2024-09-13 18:44:28 -0400

    Create regex.py

---
+++ regex.py
@@ -0,0 +1,28 @@
+""" 
+Regular Expression Engine
+Input:
+  regular expression
+  valid character set
+  input string
+Output:
+  match found (boolean)
+"""
+
+#Check if input is a string, else return error message
+
+#Check if input has valid characters, if not return error message
+
+#Read and interpret regular expression
+#handles Kleene Closure(*), Or + 
+
+#iterator to keep track of position in string
+"""
+* 0 or more (0)
+{} exact number (1)
+() grouping (0)
++ or (0)
+^ starts with (2)
+$ ends with (2)
+\escape character(2)
+"""
+
