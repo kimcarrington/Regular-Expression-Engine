9f8674ecfc8e13cad9088ee3defb69bc526a08f9
kimcarrington <165577489+kimcarrington@users.noreply.github.com>
2024-09-14 01:15:46 -0400

    Update RegularExpressionEngine.py

--- RegularExpressionEngine.py
+++ RegularExpressionEngine.py
@@ -6,6 +6,34 @@ class LexicalAnalyzer:
     self.position = 0
     self.tokens = []          #list for storing the tokens
 
-  #while loop to go through the regex and create tokens
-  while self.position < self.length:
-    
+  #method to add tokens
+  def analyze(self):
+    #while loop to go through the regex and create tokens
+    while self.position < self.length:
+      character = self.regex[self.position]
+      #handle operators
+      if character in '*+().?':
+        self.tokens.append(character)
+        self.position += 1
+      #handle escapes
+      elif character == "\\":
+        #call method to handle this case
+        self.tokens.append(self.analyzeEscape())
+      #handle literals
+      else:
+        self.tokens.append(character)
+        self.position += 1
+
+    #method to tokenize escape sequence
+    def analyzeEscape(self):
+      #check next character
+      self.positon += 1
+      character = self.regex[self.position]
+      #check if next character is supported
+      if character in '*+().?':
+        self.position += 1
+        return '\\' + character
+      #error handling
+      else:
+        raise ValueError("Unsupported Escape Sequence.")
+  
