c680d0ce2771c009a603979ed907ba8390640f9c
kimcarrington <165577489+kimcarrington@users.noreply.github.com>
2024-12-16 01:16:57 -0500

    Update LexicalAnalyzer.py

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -3,72 +3,53 @@ class LexicalAnalyzer:
     self.regex = regex
     self.length = len(regex)
     self.position = 0
-    #self.previousIndex = 0
-    self.tokens = []           #list of dictionaries
-
-#method to add tokens 
-def analyze(self):
-  #while loop to go through the regex and create tokens
-  while self.position < self.length:
-    character = self.regex[self.position]
-    #handle unary operators
-    if character in '*':
-      self.tokens.append(dict(Value = character, Type = "kleeneStar"))
-      self.position += 1
-    #handle binary operators
-    elif character in '+':
-      self.tokens.append(dict(Value = character, Type = "alternationOperator"))
-      self.position += 1
-    #handle escape characters
-    elif character == '\\':
-      #call method to analyze escape sequence
-      self.tokens.append(self.analyzeEscape())
-    #handle the beginning of a subexpression
-    elif character == '(':
-      self.tokens.append(dict(Value = character, Type = "openSubexpression"))
-      self.position += 1
-    #Handle the end of a subexpression
-    elif character == ')':
-      self.tokens.append(dict(Value = character, Type = "closeSubexpression"))
-    #handle literals
-    else:
-      self.tokens.append(dict(Value = character, Type = "literal"))
-
-#method to tokenize escape sequence
-def analyzeEscape(self):
-  #check next character
-  self.position += 1
-
-  #send error message if end of sequence is reached
-  if self.position >= self.length:
-    raise ValueError("Incomplete Escape Sequence at End of Input.")
-  character = self.regex[self.position]
-  #check if next character is supported
-  if character in '*+()':
-    self.position += 1
-    return '\\' + character
-  #send error message
-  else:
-    raise ValueError("Unsupported Escape Sequence.")
-
-#method to add concatenation to tokens
-def concatenate(self):
-  #check length of tokens
-  if len(self.tokens) >= 2:
-    self.position = 1
-    #while loop to iterate through tokens
-    while self.position < len(self.tokens):
-      #update current and previous
-      current = self.tokens[self.position]
-      previous = self.tokens[self.position - 1]
-      #conditionals
-      if (
-        (previous['Type'] == "literal" and (current['Type'] == "literal" or current['Type'] == "openSubexpression")) or 
-        (current['Type'] == "literal" and (previous['Type'] == "closeSubexpression" or previous['Type'] == "unaryOperator"))
-         ):
-           #append and update position
-           self.tokens.insert(self.position, dict(Value = "-", Type = "concat"))
-           self.position += 1
-      #increment position
-      self.position += 1
+    self.tokens = []      
+    
+    self.analyze()
+    self.concatenate()
   
+  # Function to add tokens 
+  def analyze(self):
+
+    # Iterate through regex
+    while self.position < self.length:
+      character = self.regex[self.position]
+
+      # Handle kleene star
+      if character in '*':
+        self.tokens.append(dict(Value = character, Type = "kleeneStar"))
+      # Handle alternation
+      elif character in '+':
+        self.tokens.append(dict(Value = character, Type = "alternationOperator"))
+      # Handle subexpressions
+      elif character == '(':
+        self.tokens.append(dict(Value = character, Type = "openSubexpression"))
+      elif character == ')':
+        self.tokens.append(dict(Value = character, Type = "closeSubexpression"))
+      # Handle literals
+      else:
+        self.tokens.append(dict(Value = character, Type = "literal"))
+        
+      # Increment position
+      self.position += 1
+
+  # Function to add concat nodes
+  def concatenate(self):
+    # Check for two or more tokens
+    if len(self.tokens) >= 2:
+      self.position = 1
+
+      while self.position < len(self.tokens):
+        current = self.tokens[self.position]
+        previous = self.tokens[self.position - 1]
+
+        # Check if previous and current should be concatenated
+        if (
+          (previous['Type'] == "literal" and (current['Type'] == "literal" or current['Type'] == "openSubexpression")) or 
+          (current['Type'] == "literal" and (previous['Type'] == "closeSubexpression" or previous['Type'] == "kleeneStar")) or
+          (previous['Type'] == "closeSubexpression" and current['Type'] == "openSubexpression")):
+            # Add concat node
+            self.tokens.insert(self.position, dict(Value = "-", Type = "concat"))
+            self.position += 1
+
+        self.position += 1
